4a7b63e0d9f0abfaf261063f67a7b584f20ac6c5
Termonoid <termonoid@yandex.ru>
2020-08-28 16:01:11 +0300

    completed config types, changed some names for usability

--- config.py
+++ config.py
@@ -1,23 +1,40 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 import yaml
+from mastodon import Mastodon
+import vk
 
+from .types import Publication
 
 class VKPage:
     "это откуда мы тыбзим"
-    domain: str
-    token: str
+    screen_name: str
+    api: vk.API
 
     def __init__(self, config: Dict):
+        screen_name = config.get("screen_name")
+        token = config.get("token")
+        if not all(screen_name, token):
+            raise ValueError("Invalid source definition")
+        self.screen_name = screen_name
+        self.api = vk.API(vk.Session(access_token=token), v=5.95)
+
+    def get_new(self, last_post_time: int, count: int) -> List[Publication]:
         pass
 
 
 class MastodonAccount:
     "это куда мы постим"
-    token: str
-    node_domain: str
+    api: Mastodon
 
     def __init__(self, config: Dict):
+        node_domain = config.get("domain")
+        token = config.get("token")
+        if not all(node_domain, token):
+            raise ValueError("Invalid destination definition")
+        self.api = Mastodon(access_token=token, api_base_url=node_domain)
+
+    def publish(self, post: Publication):
         pass
 
 
@@ -39,15 +56,13 @@ class Config:
     def __init__(self, file_name: str):
         with open(file_name) as file:
             config_dict: dict = yaml.safe_load(file)
-        if "global" in config_dict:
-            self.vk_token = config_dict["global"].get("vk_token", None)
 
         if "sources" in config_dict:
             for name, source in config_dict["sources"].items():
                 self.sources[name] = VKPage(source, self.vk_token)
 
         if "destinations" in config_dict:
-            for name, destination in config_dict["destinations"]:
+            for name, destination in config_dict["destinations"].items():
                 self.destinations[name] = MastodonAccount(destination)
 
         if "bridges" in config_dict:
---
+++ types.py
@@ -0,0 +1,2 @@
+class Publication:
+    pass
