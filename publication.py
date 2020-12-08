319c38654cbc1d1003cb6a3c71dff447541cf3a2
Termonoid <termonoid@yandex.ru>
2020-12-08 14:34:53 +0300

    remove hardcoded mastodon arch

--- publication.py
+++ publication.py
@@ -1,16 +1,10 @@
 from typing import List, Dict, Any
 
-from mastodon import Mastodon
-
-
 class Attachment:
     link: str
     type: str
 
     def __init__(self, object_: Dict[str, Any]):
-        ...
-
-    def upload(self, api: Mastodon) -> str:
         ...
 
 
@@ -19,10 +13,4 @@ class Publication:
     attachments: List[Attachment]
 
     def __init__(self):
-        ...
-
-    def publish(self, api: Mastodon):
-        ...
-
-    def split(self) -> List['Publication']:
         ...
