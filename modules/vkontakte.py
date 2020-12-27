4cc778fefa41177b7efda583e3753530a8d9062d
Termonoid <termonoid@yandex.ru>
2020-12-27 16:28:00 +0300

    add parse functions to module VK

--- modules/vkontakte.py
+++ modules/vkontakte.py
@@ -1,14 +1,72 @@
 from typing import Dict, Any
+import operator
 
 import vk
 
 from exceptions import InvalidConfig
-from publication import Publication, Attachment
+from publication import Publication, FileAttach, FileType, Poll
 from . import Source
 
 
-def parse_attachment(raw: Dict[str, Any]) -> Attachment:
-    ...
+class NeedExpand(Exception):
+    adding_line: str
+
+    def __init__(self, add):
+        self.adding_line = add
+
+
+class UnsupportedAttachment(Exception):
+    type: str
+
+    def __init__(self, type):
+        self.type = type
+
+
+def add_line(text: str, line: str):
+    return text + '\n' + line
+
+
+def parse_attachment(raw: Dict[str, Any]) -> FileAttach:
+    type = raw['type']
+    object = raw[type]
+    if type == 'photo':
+        return FileAttach(FileType.PICTURE, link=object['sizes'][-1]['url'])
+    if type == 'video':
+        raise NeedExpand(object['player'])
+    if type == 'audio':
+        return FileAttach(FileType.AUDIO, link=object['url'])
+    if type == 'doc':
+        return FileAttach(FileType.CUSTOM, link=object['url'])
+    if type == 'link':
+        raise NeedExpand(object['url'])
+    if type == 'album':
+        raise NeedExpand('https://vk.com/album'
+                         f'{object["owner_id"]}_{object["id"]}')
+    if type == 'poll':
+        return Poll(object['question'],
+                    list(map(operator.itemgetter('text'), object['answers'])),
+                    object['anonymous'],
+                    object['multiple']
+                    )
+    raise UnsupportedAttachment(type)
+
+
+def parse_post(raw: Dict[str, Any]) -> Publication:
+    text = raw['text']
+    attachments = []
+    for a in raw['attachments']:
+        try:
+            attachments.append(parse_attachment(a))
+        except NeedExpand as ex:
+            text = add_line(text, ex.adding_line)
+        except UnsupportedAttachment as ex:
+            id = raw['id']
+            owner_id = raw['owner_id']
+            original = f"https://vk.com/wall{owner_id}_{id}"
+            text = add_line(text, f"Unsupported attachment type '{ex.type}'."
+                                  " You may want look to original: "
+                                  f"{original}")
+    return Publication(text, attachments)
 
 
 @Source.register("vk")
