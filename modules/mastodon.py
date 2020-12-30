299e3fa041c5f079f5b13418e3d12000e5581a54
Termonoid <termonoid@yandex.ru>
2020-12-30 20:19:29 +0300

    finished module for mastodon

--- modules/mastodon.py
+++ modules/mastodon.py
@@ -1,9 +1,52 @@
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Tuple
 
 from mastodon import Mastodon
+import requests
 
-from publication import Publication
+from publication import Publication, Poll, FileAttach
 from . import Destination
+
+
+def smart_cut(text: str, max_len: int) -> Tuple[str, str]:
+    index_of_space = text.rfind(' ', 0, max_len+1)
+    if index_of_space == -1:
+        cut_index = max_len
+    else:
+        cut_index = index_of_space
+    return (text[:cut_index], text[cut_index:])
+
+
+def extract_head(post: Publication) -> Tuple:
+    if len(post.plain_text) > 500:
+        (head, tail) = smart_cut(post.plain_text, 500-len(' ->'))
+        head = Publication(head)
+        tail = Publication(tail, post.attachments)
+        return (head, tail)
+    if len(post.attachments) > 4:
+        head = post.attachments[:4]
+        tail = post.attachments[4:]
+        head = Publication(post.plain_text, head)
+        tail = Publication(attachments=tail)
+        return (head, tail)
+    for index, attach in enumerate(post.attachments):
+        if type(attach) is Poll:
+            head = Publication(attachments=[attach])
+            post.attachments.pop(index)
+            return (head, post)
+    return (post, None)
+
+
+def split(post: Publication) -> List[Publication]:
+    posts = []
+    splitting_now = post
+    while True:
+        head, tail = extract_head(splitting_now)
+        posts.append(head)
+        if tail is None:
+            break
+        else:
+            splitting_now = tail
+    return posts
 
 
 @Destination.register('mastodon')
@@ -16,8 +59,34 @@ class MastodonDestination(Destination):
         api = Mastodon(access_token=token, api_base_url=base_url)
         self.api = api
 
-    @staticmethod
-    def _split(post: Publication) -> List[Publication]:
+    def _upload_attachment(self, attach: FileAttach):
+        req = requests.post("https://mastodon.example/api/v1/media",
+                            headers={"Authorization":
+                                     f"Bearer {self.api.access_token}"},
+                            files={'file': attach.fd}
+                            )
+        if req.ok:
+            return req.json()
 
-    def publish(self, publication: Publication):
-        pass
+    def _publish_part(self, post: Publication, reply_to=None)\
+            -> Dict[str, Any]:
+        if post.attachments and type(post.attachments[0]) is Poll:
+            raw: Poll = post.attachments[0]
+            poll = self.api.make_poll(raw.variants, multiple=raw.is_multiple)
+            published = self.api.status_post(raw.title,
+                                             poll=poll,
+                                             in_reply_to=reply_to)
+        else:
+            uploaded_ids = []
+            for i in post.attachments:
+                uploaded_ids.append(self._upload_attachment(i)['id'])
+            published = self.api.status_post(post.plain_text,
+                                             media_ids=uploaded_ids)
+        return published
+
+    def publish(self, poll: Publication):
+        posts = split(poll)
+        current_id = self._publish_part(posts.pop(0))
+
+        for p in posts:
+            current_id = self._publish_part(p, current_id)
